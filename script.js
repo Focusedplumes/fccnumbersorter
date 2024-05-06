c6350d6228d0edd5cb88d1be6015e877545ed93b
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-05-05 22:09:03 -0500

    work continued

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const sortButton = document.getElementById("sort");
+
 const sortInputArray = event => {
     event.preventDefault();
     
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+    const inputValues = [
+      ...document.getElementsByClassName("values-dropdown")
+    ].map((dropdown) => Number(dropdown.value));
+
+    updateUI(inputValues);
 };
 
 sortButton.addEventListener("click", sortInputArray);
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  }); 
+ const bubbleSort = array => {
+
+ }; 
+};
